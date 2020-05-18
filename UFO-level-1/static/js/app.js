93145a480bd9d08e0e2c33d0bd08ba05a3c70673
Don Mudd <58741602+Elton-Don@users.noreply.github.com>
2020-05-18 10:13:37 -0500

    Add files via upload
    adding js with working filter

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,3 @@
-// from data.js orignal code removed
 var tableData = data;
 
 // YOUR CODE HERE!
@@ -9,13 +8,11 @@ data.forEach(function(ufoSighting) {
     var row = tbody.append("tr");
     Object.entries(ufoSighting).forEach(function([key, value]) {
       console.log(key, value);
- //     // Append a cell to the row for each value
- //     // in the weather report object
+ // Append a cell to the row for each value
       var cell = row.append("td");
       cell.text(value)
     });
   });
-
 
 // Select the button
 var button = d3.select("#filter-btn");
@@ -23,25 +20,44 @@ var button = d3.select("#filter-btn");
 // Select the form
 var form = d3.select("#datetime");
 
-//// Create event handlers 
+// Create event handlers 
 button.on("click", runEnter);
 form.on("submit",runEnter);
 
-// // Complete the event handler function for the form
+// Complete the event handler function for the form
 function runEnter() {
+ //Prevent the page from refreshing
+ d3.event.preventDefault();
 
-  // //Prevent the page from refreshing
-   d3.event.preventDefault();
-  
- // // Select the input element and get the raw HTML node
-   var inputElement = d3.select("#datetime");
+// Select the input element and get the raw HTML node
+var inputElement = d3.select("#datetime");
 
-  // // Get the value property of the input element
-   var inputValue = inputElement.property("value");
+// Get the value property of the input element
+var inputValue = inputElement.property("value");
 
-  console.log(inputValue);
-  // console.log(tableData);
+console.log(inputValue);
 
-  var filteredData = tableData.filter(data => data.datetime === inputValue);
+var filteredData = tableData.filter(data => data.datetime === inputValue);
 
-  console.log(filteredData)};
+console.log(filteredData);
+
+// remove original data
+var tbody = d3.select("tbody");
+tbody.html("")
+
+//append filtered results
+filteredData.forEach((filteredData) => {
+  var row = tbody.append("tr");
+  Object.entries(filteredData).forEach(([key, value]) => {
+  var cell = row.append("td");
+  cell.text(value);
+  });
+});
+
+};
+
+// on click function
+button.on('click', dateFilter);
+
+// write filtered data to webpage
+d3.select("form").on('submit', dateFilter);
